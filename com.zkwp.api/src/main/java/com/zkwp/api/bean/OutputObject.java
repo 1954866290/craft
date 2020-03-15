0228998f4c6f02a7124da78317be179999fbf67a
王朋 <1954866290@qq.com>
2020-03-15 12:53:03 +0800

    123

--- com.zkwp.api/src/main/java/com/zkwp/api/bean/OutputObject.java
+++ com.zkwp.api/src/main/java/com/zkwp/api/bean/OutputObject.java
@@ -12,7 +12,7 @@ import lombok.Data;
 public class OutputObject {
 	private String returnCode;
 	private String returnMessage;
-	private Map<String, String> beans;
+	private Map<String, String> bean;
 	private long searchTotals;// 搜索时需用到，表示搜索出来的结果数
 	private List<UserPublic> list;// 搜索返回前端的结果
 	
